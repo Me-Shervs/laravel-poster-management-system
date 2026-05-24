9cc592d5f47a4d428fb63466b16e0a4f4b3853de
Shervie <shervie123456789@gmail.com>
2026-05-24 23:17:41 +0800

    feat(api): refine poster routes and authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PosterController;
 use App\Http\Controllers\PosterScheduleController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Public Routes (NO LOGIN REQUIRED)
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/login', [AuthController::class, 'login']);
+
+/*
+|--------------------------------------------------------------------------
+| Protected Routes (SANCTUM REQUIRED)
+|--------------------------------------------------------------------------
+*/
 
 Route::prefix('v1')
     ->middleware('auth:sanctum')
